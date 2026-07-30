d63f528efcdc92d7073aec09982a2f389a6ce0fc
Tushar Kumar <tusharkumar1036@gmail.com>
2026-07-30 17:47:25 +0530

    variables

---
+++ 3_variables/variables.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	//var name string = "tushar"
+
+	var name string
+	name = "tushar"
+	//type infer kr leti hai
+	fmt.Println(name)
+	//	var price = 30.30
+
+	var price int
+	price = 30
+	fmt.Println(price)
+	age := 20 //infer
+	fmt.Println(age)
+
+}
