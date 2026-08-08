645f95767ef946ee47db630142bea2407135c41a
Tushar Kumar <tusharkumar1036@gmail.com>
2026-08-08 14:10:24 +0530

    synchronization

--- 22_channels/channels.go
+++ 22_channels/channels.go
@@ -2,17 +2,31 @@ package main
 
 import "fmt"
 
-func main() {
-	/*	messagechan := make(chan string)
-		messagechan <- "ping" //channels are blocking
-		msg := <-messagechan
-		fmt.Println(msg)*/
-	ch := make(chan string) // create channel
-	// sender goroutine
-	go func() {
-		ch <- "Hello" // send
-	}()
-	msg := <-ch // receive
-	fmt.Println(msg)
+// func main() {
+// 	/*	messagechan := make(chan string)
+// 		messagechan <- "ping" //channels are blocking
+// 		msg := <-messagechan
+// 		fmt.Println(msg)*/
+// 		ch := make(chan string) // create channel
+// 	// sender goroutine
+// 	go func() {
+// 		ch <- "Hello" // send
+// 	}()
+// 	msg := <-ch // receive
+// 	fmt.Println(msg)
 
+// }
+
+//synchronization using channels======
+func worker(done chan bool) {
+	fmt.Println("Working...")
+	done <- true // signal completion
+}
+func main() {
+	done := make(chan bool)
+
+	go worker(done)
+
+	<-done // wait for signal (sync point)
+	fmt.Println("Done!")
 }
