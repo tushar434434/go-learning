9397ec74bcc89b773d7f9ca513ce64eb4294e0a5
Tushar Kumar <tusharkumar1036@gmail.com>
2026-08-13 22:57:03 +0530

    channels buffer

--- 22_channels/channels.go
+++ 22_channels/channels.go
@@ -19,6 +19,7 @@ import "fmt"
 
 //synchronization using channels======
 //sync
+/*
 func worker(done chan bool) {
 	fmt.Println("Working...")
 	done <- true // signal completion
@@ -29,6 +30,59 @@ func main() {
 	<-done // wait for signal (sync point)
 	fmt.Println("Done!")
 }
-
+*/
 //sync
 //synchro
+
+//buffer channel  sin    gle channel messaging
+
+//
+/*
+func makeBuffer(size int) chan string {
+	return make(chan string, size)
+}
+
+func main() {
+	mails := makeBuffer(3)
+
+	go func() {
+		for i := 1; i <= 5; i++ {
+			mails <- fmt.Sprintf("Mail %d", i)
+		}
+		close(mails)
+	}()
+
+	for mail := range mails {
+		fmt.Println(mail)
+	}
+}*/
+
+//multiple channels
+func main() {
+	email := make(chan string, 2)
+	sms := make(chan string, 2)
+
+	// Sending to email channel
+	go func() {
+		email <- "Email 1"
+		email <- "Email 2"
+		close(email)
+	}()
+
+	// Sending to SMS channel
+	go func() {
+		sms <- "SMS 1"
+		sms <- "SMS 2"
+		close(sms)
+	}()
+
+	// Receiving from email
+	for msg := range email {
+		fmt.Println("Email:", msg)
+	}
+
+	// Receiving from SMS
+	for msg := range sms {
+		fmt.Println("SMS:", msg)
+	}
+}
