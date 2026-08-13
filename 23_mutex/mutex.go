ed4c8071bc3ab808d7613bd9aa3eac7419bb37a6
Tushar Kumar <tusharkumar1036@gmail.com>
2026-08-13 23:16:32 +0530

    mutex

---
+++ 23_mutex/mutex.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+/*
+func main() {
+	counter := 0
+	var wg sync.WaitGroup
+
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+
+		go func() {
+			defer wg.Done()
+			counter++ // Race condition
+		}()
+	}
+
+	wg.Wait()
+
+	fmt.Println("Counter:", counter)
+}*/
+//solution
+func main() {
+	counter := 0
+
+	var wg sync.WaitGroup
+	var mutex sync.Mutex
+
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+
+		go func() {
+			defer wg.Done()
+
+			mutex.Lock()
+			counter++
+			mutex.Unlock()
+		}()
+	}
+
+	wg.Wait()
+
+	fmt.Println("Counter:", counter)
+}
